bc13fbd43ded4c9b6e6fa3d591a311f69b4a096b
Luis Borjas <luisfborjas@gmail.com>
2016-08-18 01:18:50 -0400

    comments out redundant package thingy

--- init.el
+++ init.el
@@ -13,10 +13,10 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa-stable" . "http://stable.melpa.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
+;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
+;;                          ("melpa-stable" . "http://stable.melpa.org/packages/")
+;;                          ("melpa" . "http://melpa.org/packages/")))
 
 
 ;; Load and activate emacs packages. Do this first so that the
